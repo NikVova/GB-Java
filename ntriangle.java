ff14e0a019bc1f9ceb306c3f0bb201d8384c2778
Vladimir Nikolaev <isak25@mail.ru>
2022-06-17 19:20:49 +0200

    Add the solution of the problem

--- ntriangle.java
+++ ntriangle.java
@@ -1,3 +1,28 @@
 import java.util.Scanner;
 public class ntriangle {
+     public static void main(String[] args) {
+       
+        int n = InputN();
+        int res = CalcTriangle(n);
+        System.out.printf("Треугольное число для %d будет %d", n, res);
+
+     }
+     private static int CalcTriangle(int n) {
+        return n*(n + 1)/2;
+    }
+
+     public static int InputN() {
+        Scanner inputN = new Scanner(System.in);
+        System.out.printf("Введите целое число: ");
+        while(!inputN.hasNextInt()) {
+                System.out.println("Неверный ввод!!!");
+                System.out.printf("Введите целое число: ");
+                inputN = new Scanner(System.in);
+            }
+        int n = inputN.nextInt();
+        inputN.close();
+        return n;
+        }
 }
+
+
